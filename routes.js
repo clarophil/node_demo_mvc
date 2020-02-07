9b88a937c91aede8faeb26e5b7b3da578b2b2882
Philippe DEKIMPE <dkp@ecam.be>
2020-02-07 17:50:41 +0100

    in progress

--- routes.js
+++ routes.js
@@ -11,17 +11,4 @@ router.get('/user/add', userController.add);
 
 router.post('/user', userController.new);
 
-//router.put('/user/:iduser',userController.update);
-
-//router.delete('/user/:iduser', userController.remove);
-
-// Contact routes
-// router.route('/user')
-//     .get(userController.home)
-//     .post(userController.new);
-
-// router.route('/user/:iduser')
-//     .put(userController.update)
-//     .delete(userController.remove);
-
  module.exports = router;
