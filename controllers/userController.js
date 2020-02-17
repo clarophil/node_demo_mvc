8eed50c8bd45357e4c3db04f1e9744ed0378a59e
Philippe DEKIMPE <dkp@ecam.be>
2020-02-17 14:44:33 +0100

    final commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,31 +1,48 @@
-// Import contact model
 let User = require('../models/userModel');
+
 let userList = [];
 
+// List of users
 exports.userList = function (req, res) {
-    res.render('userList.ejs',{message: '', users:userList}); 
- };
+    // let user = new User('Dupont','Paul');
+    // userList.push(user);
+    res.render('userList.ejs', {users:userList});
+}
 
- exports.userAdd =  function (req, res) {
-    res.render('userAdd.ejs');
- }
-
- exports.userSave = function (req, res) {
-    let user =  new User(req.body.lastname, req.body.firstname );
-    userList.push(user);
+// Add or update one user in the list
+exports.userNew =  function(req, res) {
+    let iduser = req.body.iduser
+    let lastname =  req.body.lastname;
+    let firstname = req.body.firstname;
+    if ( iduser == -1)
+    {
+        let user = new User(lastname,firstname);
+        userList.push(user);
+    }
+    else if( iduser >=0 )
+    {
+        //userList[iduser]['firstname'] = firstname;
+        //userList[iduser]['lastname'] = lastname;
+        let user = new User(lastname,firstname);
+        userList[iduser] = user;
+    }
     console.log(userList);
     
-    res.render('userList.ejs',{message : 'user created', users: userList});
-};
-
-exports.userUpdate = function (req, res) {
-    let position = req.body.iduser;
-    user.lastname =  req.body.lastname;
-    user.firstname =  req.body.firstname;
     res.redirect('/user');
-};
+}
+
+// Send form to update user
+exports.userFormAdd = function(req, res) {
+    res.render('userAdd.ejs', {iduser:'-1', lastname:"", firstname:""});
+}
+
+// Send user form update
+exports.userFormUpdate =function (req, res) {
+    let iduser = req.params.iduser;
+    res.render('userAdd.ejs', {iduser:iduser, lastname:userList[iduser].lastname, firstname:userList[iduser].firstname});
+}
 
 exports.userRemove = function (req, res) {
-    userList.splice(req.body.iduser,1);
+    userList.splice(req.params.iduser,1);
     res.redirect('/user');
  };
--- routes.js
+++ routes.js
@@ -5,12 +5,12 @@ let router = express.Router();
 // Import contact controller
 var userController = require('./controllers/userController');
 
-router.get('/', userController.userList) ; 
+router.get('/', (request, response) => response.redirect('/user'));
 
 router.get('/user', userController.userList);
-
-router.get('/user/add', userController.userAdd);
-
-router.post('/user/new', userController.userSave);
+router.get('/user/add', userController.userFormAdd);
+router.post('/user/new', userController.userNew);
+router.get('/user/update/:iduser', userController.userFormUpdate);
+router.get('/user/delete/:iduser', userController.userRemove);
 
  module.exports = router;
