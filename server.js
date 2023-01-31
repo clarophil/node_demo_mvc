0f7393b9609a153cb360291ac7b6f619468948ce
clarophil <dkp@ecam.be>
2023-01-31 14:03:18 +0100

    fix port

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ let router = require('./routes');
 app.use('/', router);
 
 // Launch app to listen to specified port
-var port = 8000
+var port = 8880
 app.listen(port, function () { console.log('Running server on port ' + port); })
